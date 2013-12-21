7fc563b0d965657cec6d6ec15ad692f8d908a4c4
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2013-12-21 05:41:27 +0200

    upgrade

--- csrc/lua-cjson/build-mingw32.sh
+++ csrc/lua-cjson/build-mingw32.sh
@@ -1,4 +1,4 @@
-gcc strbuf.c lua_cjson.c fpconv.c -O3 -s -o ../../bin/cjson.dll -shared \
+gcc strbuf.c lua_cjson.c fpconv.c -O3 -s -o ../../bin/cjson.dll -shared -static-libgcc \
 	-Wall -pedantic \
 	-I../lua -L../../bin -llua51 \
 	-DDISABLE_INVALID_NUMBERS
