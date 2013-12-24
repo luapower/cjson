1240575b66e0fbb0a131edb2085dec9cc2dc7ff0
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2013-12-24 16:43:55 +0200

    changed binary paths in build scripts

--- csrc/lua-cjson/build-linux32.sh
+++ csrc/lua-cjson/build-linux32.sh
@@ -1,4 +1,4 @@
-gcc strbuf.c lua_cjson.c fpconv.c -O3 -s -o ../../linux/bin/cjson.so -shared \
+gcc strbuf.c lua_cjson.c fpconv.c -O3 -s -o ../../bin/linux32/cjson.so -shared \
 	-Wall -pedantic \
 	-I../lua \
 	-DDISABLE_INVALID_NUMBERS
