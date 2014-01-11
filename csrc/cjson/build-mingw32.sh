fa1fb5d55453cf02f6658ec0c0e7bac5fa962309
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-01-11 12:57:57 +0200

    moved dll to clib

--- csrc/cjson/build-mingw32.sh
+++ csrc/cjson/build-mingw32.sh
@@ -1,4 +1,4 @@
-gcc strbuf.c lua_cjson.c fpconv.c -O3 -s -o ../../bin/mingw32/cjson.dll -shared -static-libgcc \
+gcc strbuf.c lua_cjson.c fpconv.c -O3 -s -o ../../bin/mingw32/clib/cjson.dll -shared -static-libgcc \
 	-Wall -pedantic \
 	-I../lua -L../../bin/mingw32 -llua51 \
 	-DDISABLE_INVALID_NUMBERS
