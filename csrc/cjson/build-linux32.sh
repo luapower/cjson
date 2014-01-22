21ede54e42a6082e3e1dacc655e5864f47650eec
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:50:09 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/cjson/build-linux32.sh
+++ csrc/cjson/build-linux32.sh
@@ -1,4 +1,4 @@
-gcc $CFLAGS strbuf.c lua_cjson.c fpconv.c -shared -o ../../bin/linux32/clib/cjson.so \
+gcc -O2 -s -static-libgcc strbuf.c lua_cjson.c fpconv.c -shared -o ../../bin/linux32/clib/cjson.so \
 	-Wall -pedantic \
 	-I../lua \
 	-DDISABLE_INVALID_NUMBERS
--- csrc/cjson/build-mingw32.sh
+++ csrc/cjson/build-mingw32.sh
@@ -1,4 +1,4 @@
-gcc $CFLAGS strbuf.c lua_cjson.c fpconv.c -shared -o ../../bin/mingw32/clib/cjson.dll \
+gcc -O2 -s -static-libgcc strbuf.c lua_cjson.c fpconv.c -shared -o ../../bin/mingw32/clib/cjson.dll \
 	-Wall -pedantic \
 	-I../lua -L../../bin/mingw32 -llua51 \
 	-DDISABLE_INVALID_NUMBERS
