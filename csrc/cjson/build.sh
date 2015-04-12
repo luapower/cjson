91acd57c9d77ec57c54102d03320e75375364adc
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2015-04-12 04:44:13 +0300

    renamed lua -> lua-headers

--- csrc/cjson/build.sh
+++ csrc/cjson/build.sh
@@ -1,4 +1,4 @@
-gcc -c -O2 $C strbuf.c lua_cjson.c fpconv.c -I../lua \
+gcc -c -O2 $C strbuf.c lua_cjson.c fpconv.c -I../lua-headers \
 	-Wall -pedantic -DDISABLE_INVALID_NUMBERS
 gcc *.o -shared -o ../../bin/$P/clib/$D $L
 ar rcs ../../bin/$P/$A *.o
